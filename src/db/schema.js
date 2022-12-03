514bfdebaedc9399bee47b9618aa3c3d0aaf0e86
alvaro <marzo3089blanco@hotmail.com>
2022-12-03 00:11:47 -0400

    jeus

--- src/db/schema.js
+++ src/db/schema.js
@@ -1,13 +1,12 @@
 const { Client } = require('pg')
 
 const schema = new Client({
-  host: '181.115.221.0',//181.115.221.0 -- http://ec2-52-43-96-177.us-west-2.compute.amazonaws.com
+  host: '181.115.221.238',//181.115.221.0 -- http://ec2-52-43-96-177.us-west-2.compute.amazonaws.com
   port: 5432,
   database: 'dbcatastro',
   user: 'postgres',
   password: '987456',
   ssl: {
-    require: true,
     rejectUnauthorized: false
   }
 });
